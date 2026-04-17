539ce725c038651fd3f8ee243067e4c7ff60ec51
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-04-17 22:37:49 +0200

    Add help wizard when invalid command is passed

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include "update.hpp"
 #include <cstring>
+#include <iostream>
 int main(int argc, char **argv) {
   if (argc == 2 && strcmp(argv[1], "update") == 0) {
+    std::cout << "Updating program\n\n\n";
+
     UpdateProgram();
+    return 0;
   }
+
+  std::cout << "fsysutils help wizard!\n"
+            << " usage: fsysutils <command> <subcommand (possibly exists, "
+               "check command usage)> <args>\n\n"
+            << " availabe commands:\n"
+            << "   update\n"
+            << "     you guessed it, the program updates itself\n";
+
   return 0;
 }
