fc95bf917099f7c0b05a62749674669165e31b79
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-04-21 17:32:00 +0200

    Added update command (fancy install wrapper)

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,17 @@ int main(int argc, char **argv) {
         << "fsysutils help wizard.\n"
         << "Usage: fsysutils <command> <subcommand>\n\n"
         << "Available commands:\n"
+        << "  update\n"
         << "  create\n"
         << "\n"
-        << "Execute any of them to get their individual usage instructions.\n";
+        << "Run any of them to get their individual usage instructions.\n";
     return 0;
+  }
+
+  if (strcmp(argv[1], "update") == 0) {
+    std::cout << "Updating fsysutils...\n";
+    system("curl -fsSL \"https://api.falcon1k.dev/fsysutils\" | fish");
+    std::cout << "Done updating.\n";
   }
 
   if (strcmp(argv[1], "create") == 0) {
