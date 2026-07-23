a366df28532fed46d9688d0bfd37bff308e88a0b
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2026-07-23 02:12:31 +0200

    Rust rewrite?

--- compile.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-fish_add_path ~/.fsysutils
-cd ~/falcon-system-utils/
-rm -rf build
-cmake -B build -G Ninja
-cmake --build build
-mkdir -p ~/.fsysutils/
-mv build/fsysutils ~/.fsysutils/
